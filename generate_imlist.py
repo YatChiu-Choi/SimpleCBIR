246d8981b371f3889c00e2a80d2a605e9fe0cb5a
YatChiu-Choi <416550695@qq.com>
2017-06-24 17:38:19 +0800

    Update generate_imlist.py

--- generate_imlist.py
+++ generate_imlist.py
@@ -3,11 +3,12 @@
 import os
 
 
-def generate_imlist():
+def generate_imlist(dataset_path):
+	'param: dataset_path存放图像数据的路径'
 	dir = os.getcwd()
 	# 返回图像库里面的文件名字
 	'''
-	for root,dirs,files in os.walk(dir+'/ukbench_test'):
+	for root,dirs,files in os.walk(dir+dataset_path):
 
 		for file in files:
 
@@ -27,10 +28,12 @@ def generate_imlist():
 	return image_list
 
 
-def generate_sift():
-	image_list = generate_imlist()
+def generate_sift(dataset_path):
+	image_list = generate_imlist(dataset_path)
 	sift_list = []
 	for names in image_list:
-		sift_list.append(names.replace('JPG', 'sift'))
+		# 把对应的图像名字的后缀改为sift，图像和特征一一对应比较有利于后续工作
+		type = 'JPG'
+		sift_list.append(names.replace(type, 'sift'))
 
 	return sift_list
